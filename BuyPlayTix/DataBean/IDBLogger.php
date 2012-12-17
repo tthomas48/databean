d3b73be6bbbe67e7e1d33e2cb290265b474c1b35
Tim Thomas <tim@buyplaytix.com>
2012-12-17 15:31:39 -0600

    changed to interface

--- BuyPlayTix/DataBean/IDBLogger.php
+++ BuyPlayTix/DataBean/IDBLogger.php
@@ -1,6 +1,6 @@
 <?php
 namespace BuyPlayTix\DataBean;
-class IDBLogger {
+interface IDBLogger {
   function alert($message, $vars = array());
   function critical($message, $vars = array());
   function error($message, $vars = array());
