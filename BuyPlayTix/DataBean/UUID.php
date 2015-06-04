44095486355898e1e1aa5ec564ba36f365fd9d68
Tim Thomas <tim@buyplaytix.com>
2015-06-04 10:39:12 -0500

    made domain more generic

--- BuyPlayTix/DataBean/UUID.php
+++ BuyPlayTix/DataBean/UUID.php
@@ -6,7 +6,7 @@ class UUID
 {
     public static function get()
     {
-        $uuid5 = U::uuid5(U::NAMESPACE_DNS, 'secure.buyplaytix.com');
+        $uuid5 = U::uuid5(U::NAMESPACE_DNS, 'tthomas48.github.com');
         return $uuid5->toString();
     }
 }
