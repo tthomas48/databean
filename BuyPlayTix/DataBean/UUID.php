f81f9b586081fac2c3d4df52d34431bef50b6e65
Tim Thomas <tim@buyplaytix.com>
2015-06-03 21:51:38 -0500

    changed uuid implementation

--- BuyPlayTix/DataBean/UUID.php
+++ BuyPlayTix/DataBean/UUID.php
@@ -1,10 +1,12 @@
 <?php
 namespace BuyPlayTix\DataBean;
+
+use Rhumsaa\Uuid\Uuid as U;
 class UUID
 {
     public static function get()
     {
-        return uuid_create();
+        $uuid5 = U::uuid5(U::NAMESPACE_DNS, 'secure.buyplaytix.com');
+        return $uuid5->toString();
     }
 }
-?>
